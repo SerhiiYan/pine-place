3a2bf8ccca834d1ccfb530c0cd577199587b9ee4
SerhiiYan <serhii.yanchevskii@gmail.com>
2025-07-24 14:10:09 +0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// astro.config.mjs
 
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react"; 
+import icon from "astro-icon"; // Импортируем интеграцию
+
+export default defineConfig({
+  integrations: [
+    react(),
+    
+    // Добавляем интеграцию с полной и правильной конфигурацией
+    icon({
+      // Указываем, какие наборы иконок мы разрешаем использовать
+      // 'mdi' - это название набора, ['*'] - означает "все иконки из этого набора"
+      include: {
+        mdi: ['*'] 
+      },
+    })
+  ] 
+});
